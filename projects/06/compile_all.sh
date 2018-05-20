216a9cd5c98146db7ecaceaf705396ea06de0adf
David Vuong <david.vuong@uwaterloo.ca>
2018-05-19 23:33:30 -0400

    script to run assembler

---
+++ projects/06/compile_all.sh
@@ -0,0 +1,8 @@
+make
+./HackAssembler add/Add.asm
+./HackAssembler max/Max.asm
+./HackAssembler max/MaxL.asm
+./HackAssembler rect/Rect.asm
+./HackAssembler rect/RectL.asm
+./HackAssembler pong/Pong.asm
+./HackAssembler pong/PongL.asm
