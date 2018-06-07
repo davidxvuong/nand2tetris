75929a81a6e117c3afe4b00289179ec1d5c5bdc9
David Vuong <david.vuong@uwaterloo.ca>
2018-06-07 01:19:13 -0400

    prints instruction in vm file

--- projects/07/main.c
+++ projects/07/main.c
@@ -1,8 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "inc/parser.h"
 
-int main() {
-	printf("test\n");
+#define ARGV_INDEX 1
+
+#define STRING_BUFFER 256
+
+int main(int argc, char* argv[]) {
+	
+	if (argc > 2) {
+		printf("Invalid parameters\n");
+		return -1;
+	}
+
+	FILE* fp = NULL;
+
+	fp = fopen(argv[ARGV_INDEX], "r");
+
+	if (fp == NULL) {
+		printf("Unable to open file\n");
+		return -1;
+	}
+
+	while(!feof(fp)) {
+		char* buf = (char*)(malloc(sizeof(char) * STRING_BUFFER));
+		fgets(buf, STRING_BUFFER, fp);
+
+		printf("%s", buf);
+	}
+
+	fclose(fp);
+	fp = NULL;
 
 	return 0;
 }
